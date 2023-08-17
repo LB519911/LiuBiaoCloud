c8d69357d3f0020f10997aea2754713c5154dbf5
liubiao <liubiaozhgm@gmail.com>
2023-08-17 16:20:26 +0800

    SuspendProcessInstance完成

--- web-and-app/ruoyi-ui/src/views/school/startSchool/index.vue
+++ web-and-app/ruoyi-ui/src/views/school/startSchool/index.vue
@@ -114,6 +114,7 @@
             type="text"
             style="color: #ee4f4f"
             icon="el-icon-s-comment"
+            @click="suspendProcessInstance(scope.row)"
             v-hasPermi="['school:school:edit']">取消申请
           </el-button>
           <el-button
@@ -216,7 +217,8 @@ import {
   updateSchool,
   startFlow,
   hiFlow,
-  hiFlowText
+  hiFlowText,
+  suspendProcessInstance
 } from "@/api/school/school";
 
 export default {
@@ -376,6 +378,16 @@ export default {
       });
       hiFlowText(id).then(response => {
         this.hiFlowText = response.data
+      });
+    },
+    /** 取消申请 */
+    suspendProcessInstance(row) {
+      this.reset();
+      const id = row.id || this.ids
+      suspendProcessInstance(id).then(response => {
+        this.$modal.msgSuccess("申请已取消");
+        this.open = false;
+        this.getList();
       });
     },
     /** 提交按钮 */
